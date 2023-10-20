c71290ab76911ed8b478beec93955800fdb490f5
FHenric <ffilipehhenrique@gmail.com>
2023-10-20 20:46:33 -0300

    Initializing the exercise

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter account data:");
+        System.out.print("Number: ");
+
         String teste = sc.nextLine();
         System.out.println(teste);
 
--- src/models/Account.java
+++ src/models/Account.java
@@ -6,6 +6,45 @@ public class Account {
     Double balance;
     Double withdrawLimit;
 
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public Double getWithdrawLimit() {
+        return withdrawLimit;
+    }
+
+    public void setWithdrawLimit(Double withdrawLimit) {
+        this.withdrawLimit = withdrawLimit;
+    }
+
+    public Account(Integer number, String holder, Double balance, Double withdrawLimit) {
+        this.number = number;
+        this.holder = holder;
+        this.balance = balance;
+        this.withdrawLimit = withdrawLimit;
+    }
+
     public void deposit(Double amount){
 
     }

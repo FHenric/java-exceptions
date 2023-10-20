9a5496ca4bf0239cc178412187b28fda490bf9c3
FHenric <ffilipehhenrique@gmail.com>
2023-10-20 20:22:44 -0300

    inicialização do exercicio pronta

---
+++ src/models/Account.java
@@ -0,0 +1,17 @@
+package models;
+
+public class Account {
+    Integer number;
+    String holder;
+    Double balance;
+    Double withdrawLimit;
+
+    public void deposit(Double amount){
+
+
+    }
+
+    public void withdraw(Double amount){
+
+    }
+}
